e2712237aee4a05f0433fca1441a75f41e94078e
Simon Barth <6423657+Data5tream@users.noreply.github.com>
2026-07-20 16:59:22 +0000

    machfile: add 0.2.2 bottle.

--- Formula/machfile.rb
+++ Formula/machfile.rb
@@ -5,6 +5,12 @@ class Machfile < Formula
   sha256 "5e40d3fd6b9083f2b59f31f4043f6562b1f026ff51ae1c771583abe99df97e17"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/machfile/homebrew-tap/releases/download/machfile-0.2.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "8b2a61f86e8e932759abcba50b020b9153a59a76c384138ae5621d0010fdc6ad"
+    sha256 cellar: :any,                 x86_64_linux: "6ed13d9e5ccd9df52e63e66bca59e9196fda9e8d50fd4481af9e9d64b604222f"
+  end
+
   depends_on "rust" => :build
 
   def install
